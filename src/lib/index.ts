fa19d3bfe2642e99b1ff53974c7d0cec6a99a414
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-02-05 15:34:24 +0100

    feat: create index file for library exports

---
+++ src/lib/index.ts
@@ -0,0 +1,13 @@
+/*
+ * ############################################################################### *
+ * Created Date: We Feb 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Wed Feb 05 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ############################################################################### *
+ */
+export * from './utils'
