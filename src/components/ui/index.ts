d97bba5b56982d407e75b5fa0c086f510374440d
bermuda <olasunkanmiboluwatife110@gmail.com>
2025-02-05 15:04:16 +0100

    feat: export multiple UI components from centralized index file

---
+++ src/components/ui/index.ts
@@ -0,0 +1,24 @@
+/*
+ * ############################################################################### *
+ * Created Date: We Feb 2025                                                   *
+ * Author: Boluwatife Olasunkanmi O.                                           *
+ * -----                                                                       *
+ * Last Modified: Wed Feb 05 2025                                              *
+ * Modified By: Boluwatife Olasunkanmi O.                                      *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ############################################################################### *
+ */
+export * from './button'
+export * from './checkbox'
+export * from './dialog'
+export * from './input'
+export * from './label'
+export * from './scroll-area'
+export * from './select'
+export * from './separator'
+export * from './sheet'
+export * from './skeleton'
+export * from './textarea'
+export * from './tooltip'
